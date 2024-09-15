fcc63c34cdf95805ab6e11b67f51ce5a0158574a
shaamocha <mochashaa30september2003@gmail.com>
2024-09-15 17:18:07 +0700

    main

--- js/ebook.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.skill__card').forEach(card => {
-    card.addEventListener('click', () => {
-        alert(`You clicked on ${card.querySelector('h3').textContent}`);
-    });
-  });
