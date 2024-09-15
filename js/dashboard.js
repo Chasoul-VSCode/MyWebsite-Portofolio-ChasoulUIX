086434f2716c5e737635232012b91d00185102ac
shaamocha <mochashaa30september2003@gmail.com>
2024-09-15 12:46:59 +0700

    main

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,3 +3,30 @@ document.querySelectorAll('.skill__card').forEach(card => {
         alert(`You clicked on ${card.querySelector('h3').textContent}`);
     });
   });
+
+     // Fungsi untuk mendeteksi apakah elemen dalam viewport
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  // Fungsi untuk menambahkan atau menghapus kelas 'scroll-active'
+  function animateOnScroll() {
+    const zoomElements = document.querySelectorAll('.zoom-in');
+    
+    zoomElements.forEach(function(el) {
+      if (isElementInViewport(el)) {
+        el.classList.add('scroll-active');
+      }
+    });
+  }
+  
+  // Trigger fungsi saat halaman di-scroll atau dimuat
+  window.addEventListener('scroll', animateOnScroll);
+  window.addEventListener('load', animateOnScroll);
+  
